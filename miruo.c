a3b2973a5341c629fc35a6c467e8f5a1372fa151
Yasui <yasui0906@gmail.com>
2011-07-05 15:19:59 +0900

    --

--- miruo.c
+++ miruo.c
@@ -17,22 +17,22 @@ void usage()
   printf("  option\n");
   printf("   -h, --help                     # help\n");
   printf("   -V, --version                  # version\n");
-  printf("       --all                      # all session lookup\n");
-  printf("       --live                     # live mode(all segment lookup)\n");
-  printf("   -q, --qiute                    # \n");
-  printf("   -F, --flagment=[0|1]           # ip flagment lookup. default=1\n");
-  printf("   -C, --color=[0|1]              # color 0=off 1=on\n");
-  printf("   -S, --syn=[0|1]                # syn retransmit lookup mode.default=1. 0=ignore 1=lookup\n");
-  printf("   -R, --rst=[0|1|2]              # rst lookup mode.default=1. (see README)\n");
+  printf("   -i, --interface=dev            # eth0,bond0,any...\n");
   printf("   -v, --view-data=NUM            # \n");
-  printf("   -L, --limit-session=NUM        # active session limit. Default 1024\n");
-  printf("   -l, --limit-segment=NUM        # active segment limit. Default 65536\n");
   printf("   -T, --connection-time=time[ms] # Threshold of connection time for lookup. Default 0ms(off)\n");
   printf("   -t, --long-delay-time=time[ms] # Threshold of long delay time for lookup. Default 0ms(off)\n");
   printf("   -r, --retransmit-time=time[ms] # Threshold of retransmit time for lookup. Default 1000ms\n");
   printf("   -s, --stat=interval[sec]       # statistics view interval. Default 0sec(off)\n");
   printf("   -f, --file=file                # read file(for tcpdump -w)\n");
-  printf("   -i, --interface=dev            # eth0,bond0,any...\n");
+  printf("   -S, --syn=[0|1]                # syn retransmit lookup mode.default=1. 0=ignore 1=lookup\n");
+  printf("   -R, --rst=[0|1|2]              # rst lookup mode.default=1. (see README)\n");
+  printf("   -F, --flagment=[0|1]           # ip flagment lookup. default=1\n");
+  printf("   -C, --color=[0|1]              # color 0=off 1=on\n");
+  printf("   -L, --limit-session=NUM        # active session limit. Default 1024\n");
+  printf("   -l, --limit-segment=NUM        # active segment limit. Default 65536\n");
+  printf("   -q, --qiute                    # \n");
+  printf("       --all                      # all session lookup\n");
+  printf("       --live                     # live mode(all segment lookup)\n");
   printf("\n");
   printf("  expression: see man tcpdump\n");
   printf("\n");
@@ -1033,18 +1033,20 @@ tcpsession *miruo_tcpsession_destroy(tcpsession *c, int view, char *msg, char *r
 
 void miruo_tcpsession_timeout()
 {
+  int d;
   tcpsession *c;
   tcpsegment *s;
 
   c = opt.tsact;
-  if(c == NULL){
-    return;
-  }
   while(c){
     s = c->last ? c->last : &(c->segment);
-    if((opt.ntv.tv_sec - s->ts.tv_sec) > 900){
+    d = opt.ntv.tv_sec - s->ts.tv_sec;
+    if(d > 900){
+      opt.count_ts_timeout++;
+      c = miruo_tcpsession_destroy(c, 1, "destroy session", "time out");
+      continue;
     }
-    if((opt.ntv.tv_sec - s->ts.tv_sec) > 30){
+    if(d > 30){
       switch(c->st[0]){
         case MIRUO_STATE_TCP_SYN_SENT:
         case MIRUO_STATE_TCP_SYN_RECV:
@@ -1073,12 +1075,12 @@ void miruo_tcpsession_timeout()
       if(s->view){
         // 再送時間の最長を暫定的に30秒として、それ以前に受け取ったセグメントを破棄する。
         // この処理の目的は、メモリを節約することと、セッション開放時の処理を軽減させること。
-        // 大量のセグメントを保持し続けてもいいことないので仕方なく・・・
         // そのため30秒以内の再送は検出できるがそれ以上かかった場合は検知できなくなる。
         // まあ、30秒でも十分に大きすぎる気がするのでRTOをざっくり計算したほうがいいのかな。
         if((opt.ntv.tv_sec - s->ts.tv_sec) > 30){
         }
       }
+      s = s->next;
     }
     c = c->next;
   }
